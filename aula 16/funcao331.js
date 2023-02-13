a07bca65462cbec1fae5655f9c0c74ad38978760
luckomatsu <luckomatsu@gmail.com>
2023-02-13 17:29:08 -0300

    organizando e atualizando

---
+++ aula 16/funcao331.js
@@ -0,0 +1,11 @@
+function parimpar(n) {
+    if (n%2==0){
+        return 'par!'
+    } else {
+        return 'Ímpar!'
+    }   
+}
+
+let res = parimpar(4)   // console.log(parimpar(4))
+console.log(res)
+
