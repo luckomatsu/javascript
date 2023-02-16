556b9adf48e0443dcc18613d3fa9d3e79ca304ce
luckomatsu <luckomatsu@gmail.com>
2023-02-16 13:58:28 -0300

    exercício contador

--- aula 14/ex016/script.js
+++ aula 14/ex016/script.js
@@ -1,1 +1,27 @@
-alert('Olá!')
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (i < f) {
+            //Contagem crescente
+            for(let c= i; c <= f; c += p) {
+            res.innerHTML += ` ${c} \u{1F449}`
+            }
+         } else {
+            //Contagem Regressiva
+            for(let c = i; c >= f; c-= p) {
+                res.innerHTML += ` ${C} \U{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
